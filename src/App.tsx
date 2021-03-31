ad6efb856ace13134da84a30664e56ca65ae3f93
Mike Solis <fullstacksolis@gmail.com>
2021-03-30 20:42:39 -1000

    renamed Onboarding handler

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ const App: React.FC = () => {
                 return tutorialCompleted ? (
                   <Home />
                 ) : (
-                  <Onboarding getStartedHandler={setTutorialCompleted} />
+                  <Onboarding completedTutorialHandler={setTutorialCompleted} />
                 );
               }}
             />
